ba33cef9d67e9b022f5b18c10cb462c948c74954
cyuanyuan520 <482734085@qq.com>
2024-11-02 10:32:23 +0800

    将邮箱验证码改为使用redis缓存

--- src/main/java/com/itheima/reggie/config/RedisConfig.java
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.config;
 
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
 
+    @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         //更换key序列化器
--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -8,12 +8,15 @@ import com.itheima.reggie.service.UserService;
 import com.itheima.reggie.utils.SMSUtils;
 import com.itheima.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.jsqlparser.statement.select.KSQLJoinWindow;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RequestMapping("/user")
 @RestController
@@ -22,6 +25,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session){
@@ -32,11 +37,14 @@ public class UserController {
         }
         //获取四位验证码
         String code = ValidateCodeUtils.generateValidateCode(4).toString();
-        log.info("code: {}", code);//偷看验证码
+        log.info("偷看验证码 code: {}", code);//偷看验证码
         //发送验证码
         SMSUtils.sendMessage(phone, code);
-        //将发送成功的验证码存在session中
-        session.setAttribute(phone, code);
+//        //将发送成功的验证码存在session中
+//        session.setAttribute(phone, code);
+
+        //将发送的验证码保存在redis中 key是注册人的邮箱 v即为四位验证码
+        redisTemplate.opsForValue().set(phone, code, 5L, TimeUnit.MINUTES);
         return R.success("发送成功!");
     }
 
@@ -50,8 +58,11 @@ public class UserController {
         //获取用户输入的验证码
         String code = map.get("code").toString();
 
-        //获取实际上的验证码
-        String realCode = (String) session.getAttribute(phone);
+//        //从session获取实际上的验证码
+//        String realCode = (String) session.getAttribute(phone);
+
+        //从redis取出实际上的验证码
+        String realCode = (String) redisTemplate.opsForValue().get(phone);
 
         //比对验证码
         if (realCode == null || realCode.equals(code)) {
@@ -67,9 +78,12 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user", user.getId());
+
+            //用户登录成功 销毁redis中对应的数据
+            redisTemplate.delete(phone);
             return R.success(user);
         }
-        return R.error("登录错误..");
+        return R.error("验证码填写错误!");
     }
 
 
